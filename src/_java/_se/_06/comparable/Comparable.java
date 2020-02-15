8713376ca7cfb4d4bad47400bc58fc2ea65239f2
aidostraining <60025201+aidostraining@users.noreply.github.com>
2020-02-15 10:13:29 +0600

    Comparable example added

---
+++ src/_java/_se/_06/comparable/Comparable.java
@@ -0,0 +1,10 @@
+package _java._se._06.comparable;
+
+/**
+ * Created by user on 15.02.2020.
+ */
+public interface Comparable {
+    public void setFirstName(String firstName);
+    public void setLastName(String lastName);
+    public void setAge(int age);
+}
---
+++ src/_java/_se/_06/comparable/Testing.java
@@ -0,0 +1,46 @@
+package _java._se._06.comparable;
+
+import java.util.Arrays;
+
+/**
+ * Created by user on 15.02.2020.
+ */
+public class Testing {
+    public static void main(String[] args){
+        Person[] persons = new Person[4];
+        persons[0] = new Person();
+        persons[0].setFirstName("Elvis");
+        persons[0].setLastName("Goodyear");
+        persons[0].setAge(56);
+        persons[1] = new Person();
+        persons[1].setFirstName("Stanley");
+        persons[1].setLastName("Clark");
+        persons[1].setAge(8);
+        persons[2] = new Person();
+        persons[2].setFirstName("Jane");
+        persons[2].setLastName("Graff");
+        persons[2].setAge(16);
+        persons[3] = new Person();
+        persons[3].setFirstName("Nancy");
+        persons[3].setLastName("Goodyear");
+        persons[3].setAge(69);
+        System.out.println("Natural Order");
+        for (int i = 0; i < 4; i++){
+            Person person = persons[i];
+            String lastName = person.getLastName();
+            String firstName = person.getFirstName();
+            int age = person.getAge();
+            System.out.println(lastName + ", " + firstName + ". Age:" + age);
+        }
+        Arrays.sort(persons);
+        System.out.println();
+        System.out.println("Sorted by age");
+        for (int i = 0; i < 4; i++){
+            Person person = persons[i];
+            String lastName = person.getLastName();
+            String firstName = person.getFirstName();
+            int age = person.getAge();
+            System.out.println(lastName + ", " + firstName + ". Age:" + age);
+        }
+    }
+}
